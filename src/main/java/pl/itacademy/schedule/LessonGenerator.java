ac1dbd1b53873724aa5f34aba48978c54021b3ec
itacademy <itacademykrakow@gmail.com>
2020-01-20 18:32:20 +0100

    Project task: - added unit test for ScheduleGenerator; - started with implementation.

--- src/main/java/pl/itacademy/schedule/LessonGenerator.java
+++ src/main/java/pl/itacademy/schedule/LessonGenerator.java
@@ -1,7 +1,30 @@
 package pl.itacademy.schedule;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
+
 public class LessonGenerator {
     public Schedule generateSchedule(LessonParameters lessonParameters) {
-        return null;
+        long completeMinutes = 0;
+        long lessonLength = MINUTES.between(lessonParameters.getBeginTime(), lessonParameters.getEndTime());
+        long requiredMinutes = lessonParameters.getNumberOfHour() * 60;
+        LocalDate currentDate = lessonParameters.getStartDate();
+        Set<DayOfWeek> lessonDays = lessonParameters.getLessonDays();
+        List<Lesson> lessons = new ArrayList<>();
+        do {
+            while (!lessonDays.contains(currentDate.getDayOfWeek())) {
+                currentDate = currentDate.plusDays(1);
+            }
+            Lesson lesson = new Lesson(currentDate, lessonParameters.getBeginTime(), lessonParameters.getEndTime());
+            lessons.add(lesson);
+            completeMinutes = completeMinutes + lessonLength;
+        } while (requiredMinutes > completeMinutes);
+
+        return new Schedule(lessons, true);
     }
 }
--- src/test/java/pl/itacademy/schedule/LessonGeneratorTest.java
+++ src/test/java/pl/itacademy/schedule/LessonGeneratorTest.java
@@ -1,21 +1,28 @@
 package pl.itacademy.schedule;
 
-import jdk.jfr.StackTrace;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Set;
 
 import static java.time.DayOfWeek.MONDAY;
 import static java.time.DayOfWeek.WEDNESDAY;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 class LessonGeneratorTest {
-    //TODO: make this test compilable
+    private LessonGenerator lessonGenerator;
+
+    @BeforeEach
+    public void setup(){
+        lessonGenerator = new LessonGenerator();
+    }
 
     @Test
     public void generateSchedule_successfullyGeneratesProperSchedule() {
-        //TODO: finish test method
         LessonParameters lessonParameters = new LessonParameters.Builder(
                 LocalTime.of(17, 0),
                 LocalTime.of(18, 30),
@@ -25,5 +32,15 @@ class LessonGeneratorTest {
                 .fileName("schedule.xlsx")
                 .showHelp(false)
                 .build();
+
+        LocalTime beginTime = LocalTime.of(17, 0);
+        LocalTime endTime = LocalTime.of(18, 30);
+        Lesson lesson1 = new Lesson(LocalDate.of(2020, 2, 3), beginTime, endTime);
+        Lesson lesson2 = new Lesson(LocalDate.of(2020, 2, 5), beginTime, endTime);
+
+        Schedule expectedSchedule = new Schedule(List.of(lesson1, lesson2), true);
+        Schedule resultSchedule = lessonGenerator.generateSchedule(lessonParameters);
+
+        assertThat(resultSchedule, equalTo(expectedSchedule));
     }
 }
